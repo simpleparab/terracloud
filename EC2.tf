0dbde63f8b316af78d203da13f81c7fd24b0d44f
simpleparab <71583881+simpleparab@users.noreply.github.com>
2020-09-25 20:11:15 +0530

    Create EC2.tf

---
+++ EC2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "TF-Cloud" {
+  ami = "ami-0e306788ff2473ccb"
+  instance_type = "t2.micro"
+  key_name =  "parab-terraform"
+ # vpc_security_group_ids = ["sg-08e140487c45cdd39"]
+  tags = {
+    Name = "TF-Cloud"
+  }
+
+}
